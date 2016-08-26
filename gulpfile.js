eff919df1e6cad0b1c61292d5368fb6da6a86c26
lazlojuly <lazlo.gy@gmail.com>
2016-08-26 12:33:44 +0100

    chore(babel): remove babel (currently does not support class.extends)

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,21 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 const gulp = require('gulp')
-const babel = require('gulp-babel')
-// const mocha = require('gulp-mocha')
 const runSequence = require('run-sequence')
-const del = require('del')
 
 const config = {
 	src: './src/**/*.js',
-	build: './build',
-	test: './build/test/**/*.spec.js',
-	babel: {
-		presets: ['latest'],
-	},
+	test: './src/test/**/*.js',
 }
 
-
-gulp.task('develop', () => {
+gulp.task('test-watch', () => {
 	runSequence(
-		'watch'
-		// TODO add lint
+		'test',
+		() => { gulp.watch([config.src, config.test], ['test']) }
 	)
 })
 
-gulp.task('default', () => {
-	runSequence(
-		'clean',
-		'babel'
-	)
-})
-
-
-// helpers
-gulp.task('clean', () => {
-	return del(config.build)
-})
-
-gulp.task('watch', () => {
-	gulp.watch(config.src, ['babel'])
-})
-
-gulp.task('babel', () => {
-	return gulp.src(config.src)
-		.pipe(babel(config.babel))
-		.pipe(gulp.dest(config.build))
+gulp.task('test', () => {
+	gulp.src(config.test, { read: false })
+		.pipe(mocha({ reporter: 'spec' }))
+		.on('error', err => console.error(err.toString()))
 })
